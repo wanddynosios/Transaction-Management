e229903e82f7213e91938fa30a20da3726d7636c
Daniel Leibovitch <coding@leibovitch.de>
2020-08-11 15:47:58 +0200

    merging...

---
+++ discreteEventSimulation/src/main/java/de/fhdw/tm/db_simulator/model/IO_Controller.java
@@ -0,0 +1,9 @@
+package model;
+
+import model.basic.Range;
+
+public interface IO_Controller extends IO_Element{
+
+	void attachDevices(IO_Device... devices);
+
+}
---
+++ discreteEventSimulation/src/main/java/de/fhdw/tm/db_simulator/model/process/TestProcess1.java
@@ -0,0 +1,4 @@
+package model.process;
+
+public class TestProcess1 {
+}
